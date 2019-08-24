880db97f18c0467f69ac51b71c4249a3e80e497a
Abdel <789@holbertonschool.com>
2019-08-24 00:14:52 +0000

    Fix task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,22 +2,16 @@
 #Creation of files and others for web static
 sudo apt-get update -y
 sudo apt-get install nginx -y
-sudo rm -f /data/web_static/current
-sudo mkdir data
-sudo cd data
-sudo mkdir web_static
-sudo cd web_static
-sudo mkdir releases shared
-sudo cd releases
-sudo mkdir test
+sudo mkdir data/web_static/releases/test/
+sudo mkdir data/web_static/shared/
 sudo echo "<html>
          <head>
          </head>
          <body>
              Holberton School
          </body>
-      </html>" | /data/web_static/releases/test/index.html
-sudo ln -s  /data/web_static/releases/test/ /data/web_static/current
+      </html>" > /data/web_static/releases/test/index.html
+sudo ln -nsf  /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "40i location /hbnb_static/ {\n\talias /data/web_static/current;\n\t}" /etc/nginx/sites-enable/default 
+sudo sed -i '72i\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enable/default 
 sudo service nginx restart
