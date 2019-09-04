b1f81c05e9cde6e9140a00e3ddb51e5e7be79513
Giovanny Perez <789@holbertonschool.com>
2019-09-04 02:10:49 +0000

    TAsk 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" start a Flask Web application
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    list_state = storage.all(State).values()
+    print(list_state)
+    return render_template('7-states_list.html', list_state=list_state)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
