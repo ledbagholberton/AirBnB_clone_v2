8255bf6e29c3022a56ef30a3c864002f20b1296a
ledbagholberton <789@holbertonschool.com>
2019-09-04 04:35:22 +0000

    Task 7 without mistakes

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,10 +16,10 @@ def states_list():
     return render_template('7-states_list.html', list_state=list_state)
 
 
-"""@app.teardown_appcontext
+@app.teardown_appcontext
 def teardown_db(exit):
     """ Close storage """
     storage.close()
-"""
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
