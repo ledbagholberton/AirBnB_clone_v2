c8017e61b3987b352381e3bb8faf94edf5314694
Giovanny Perez <789@holbertonschool.com>
2019-09-04 02:26:31 +0000

    Add teardown

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -14,6 +14,7 @@ def states_list():
     print(list_state)
     return render_template('7-states_list.html', list_state=list_state)
 
+
 @app.teardown_appcontext
 def teardown_db():
     storage.close()
